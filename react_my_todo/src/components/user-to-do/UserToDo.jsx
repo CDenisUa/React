629436ab09ec723ab3f935a46c82dd5ea46996e5
CDenisLaptop <chepigadeveloper@gmail.com>
2021-03-01 02:27:15 +0200

    finish main layout

--- react_my_todo/src/components/user-to-do/UserToDo.jsx
+++ react_my_todo/src/components/user-to-do/UserToDo.jsx
@@ -1,11 +1,14 @@
+import { FaTimes } from "react-icons/fa";
+import { FaPlus } from "react-icons/fa";
+import cx from "classnames";
 import styles from './UserToDo.module.scss';
 
 const UserToDo = () => (
     <div className={styles.userAdd}>
         <input className={styles.input} />
         <div>
-            <span>add</span>
-            <span>delete</span>
+            <FaPlus className={cx(styles.icon, styles.add)} />
+            <FaTimes className={cx(styles.icon, styles.delete)} />
         </div>
     </div>
 );
