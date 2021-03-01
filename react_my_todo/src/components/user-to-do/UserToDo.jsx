2522feb06823b835602a5424d0294cb1650cea95
CDenisLaptop <chepigadeveloper@gmail.com>
2021-03-01 02:55:01 +0200

    add

--- react_my_todo/src/components/user-to-do/UserToDo.jsx
+++ react_my_todo/src/components/user-to-do/UserToDo.jsx
@@ -1,16 +1,44 @@
+import { React, useState } from 'react';
 import { FaTimes } from "react-icons/fa";
 import { FaPlus } from "react-icons/fa";
 import cx from "classnames";
 import styles from './UserToDo.module.scss';
 
-const UserToDo = () => (
-    <div className={styles.userAdd}>
-        <input className={styles.input} />
-        <div>
-            <FaPlus className={cx(styles.icon, styles.add)} />
-            <FaTimes className={cx(styles.icon, styles.delete)} />
-        </div>
-    </div>
-);
+const UserToDo = () => {
+    const [prevInputValue, setInputValue] = useState('');
+    const [prevListTodo, setListTodo] = useState({
+        id: '',
+        name: '',
+        done: false,
+    });
+    return (
+        <>
+            <div className={styles.userAdd}>
+                <input
+                    className={styles.input}
+                    onChange={
+                        (e) => {
+                            setInputValue(e.target.value);
+                        }
+                    }
+                />
+                <div>
+                    <FaPlus
+                        className={cx(styles.icon, styles.add)}
+                        onClick={() => {
+                            console.log('add')
+                        }}
+                    />
+                    <FaTimes
+                        className={cx(styles.icon, styles.delete)}
+                        onClick={() => {
+                            console.log('delete');
+                        }}
+                    />
+                </div>
+            </div>
+            <div className={styles.addContainer}></div>
+        </>);
+};
 
 export default UserToDo;
