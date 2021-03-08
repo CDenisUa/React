ad6219be80766daf2ee1180c7180ee0c40c87e81
CDenis <Denclub2013@gmail.com>
2021-03-08 20:22:48 +0200

    add and delete todo

--- react_my_todo/src/components/user-to-do/UserToDo.jsx
+++ react_my_todo/src/components/user-to-do/UserToDo.jsx
@@ -1,43 +1,64 @@
 import { React, useState } from 'react';
-import { FaTimes } from "react-icons/fa";
-import { FaPlus } from "react-icons/fa";
-import cx from "classnames";
 import styles from './UserToDo.module.scss';
 
 const UserToDo = () => {
     const [prevInputValue, setInputValue] = useState('');
-    const [prevListTodo, setListTodo] = useState({
-        id: '',
-        name: '',
-        done: false,
-    });
+    const [myTasks, setMytasks] = useState([
+        { id: 1, name: 'Изучить UseRef', done: false },
+        { id: 2, name: 'Написать TODO', done: false },
+        { id: 3, name: 'Прочитать книгу по JS', done: false },
+        { id: 4, name: 'Выйти прогуляться', done: false }
+    ]);
+    console.log(myTasks)
     return (
         <>
             <div className={styles.userAdd}>
                 <input
                     className={styles.input}
+                    value={prevInputValue}
                     onChange={
                         (e) => {
-                            setInputValue(e.target.value);
+                            setInputValue(e.target.value)
                         }
                     }
                 />
                 <div>
-                    <FaPlus
-                        className={cx(styles.icon, styles.add)}
+                    <button
+                        className='add'
                         onClick={() => {
-                            console.log('add')
+                            setMytasks([
+                                ...myTasks,
+                                {
+                                    id: Math.random(),
+                                    name: prevInputValue,
+                                    done: false
+                                }
+                            ]
+                            )
                         }}
-                    />
-                    <FaTimes
-                        className={cx(styles.icon, styles.delete)}
-                        onClick={() => {
-                            console.log('delete');
-                        }}
-                    />
+                    >
+                        Add
+                    </button>
                 </div>
             </div>
-            <div className={styles.addContainer}></div>
+            <div className={styles.addContainer}>
+                <ul className={styles.todolist}>
+                    {myTasks.map(task => (
+                        <li className={styles.todo} key={task.id}>
+                            <span>{task.name}</span>
+                            <button
+                                onClick={() => setMytasks(
+                                    myTasks.filter(item => {
+                                        return item && item.id !== task.id
+                                    })
+                                )}
+                            >
+                                delete
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </>);
 };
 
