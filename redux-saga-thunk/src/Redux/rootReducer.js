0c884fe832eac60c8e05c0ef7d49905688c3edd0
CDenis <lsjIk29s3Popp>
2021-05-24 20:46:25 +0300

    Delete empty projects

--- redux-saga-thunk/src/Redux/rootReducer.js
+++
@@ -1,5 +0,0 @@
-import {combineReducers} from 'redux';
-import {nav} from './reducers/nav';
-export default combineReducers({
-    nav,
-});
--- redux-start/src/Redux/rootRedusers.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import {classCounterReducer} from './classCounterReducer';
-import {hookCounterReducer} from './hookCounterReducer';
-
-export default combineReducers({
-    classCounterReducer,
-    hookCounterReducer,
-});
