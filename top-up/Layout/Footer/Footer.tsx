03f4b11fcaeb94e4018ea25711e445356130e4ef
denys.chepiha <denys.chepiha@rmgmedia.com>
2021-07-06 16:23:29 +0300

    Create footer layout and grid

--- top-up/Layout/Footer/Footer.tsx
+++ top-up/Layout/Footer/Footer.tsx
@@ -1,11 +1,16 @@
-import {FooterProps} from './Footer.props';
+import { FooterProps } from './Footer.props';
 import cn from 'classnames';
 import styles from './Footer.module.css';
+import { format } from "date-fns";
 
-export const Footer = ({...props}:FooterProps):JSX.Element => {
+export const Footer = ({className,...props}:FooterProps):JSX.Element => {
     return (
-        <div {...props}>
-            Footer
-        </div>
+        <footer className={cn(className, styles.footer)} {...props}>
+            <div>
+                OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены
+            </div>
+            <a href="#" target="blank">Пользовательское соглашение</a>
+            <a href="#" target="blank">Политика конфиденциальности</a>
+        </footer>
     );
 };
