6e93dc7ba1fdda0d46cbc34371449b390a3d8c11
denys.chepiha <denys.chepiha@rmgmedia.com>
2021-07-08 12:23:18 +0300

    Create static pages and use getStaticProps, getStaticPaths

--- top-up/pages/index.tsx
+++ top-up/pages/index.tsx
@@ -1,12 +1,15 @@
-import {Htag, Button, Text, Teg, Rating} from "../components";
-import {useState} from "react";
-import {withLayout} from "../Layout/Layout/Layout";
+import { Htag, Button, Text, Teg, Rating} from "../components";
+import { useState } from "react";
+import { withLayout } from "../Layout/Layout/Layout";
+import { GetStaticProps } from "next";
+import axios from 'axios';
+import { MenuItem } from "../interfaces/menu.interface";
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
     const [rating, setRating] = useState<number>(4);
 
     return (
-        <div>
+        <>
             <Htag tag='h1'>Htag component</Htag>
             <Button appearance='primary' arrow='right' >Click</Button>
             <Button appearance='ghost' arrow='right' >Button</Button>
@@ -17,8 +20,29 @@ function Home(): JSX.Element {
             <Teg color='primary' href='https://youtube.com'>Youtube</Teg>
             <Teg color='green'>Green button</Teg>
             <Rating rating={rating} isEditable setRating={setRating} />
-        </div>
+            <ul>
+                { menu.map(item => <li key={item._id.secondCategory}>{item._id.secondCategory}</li>)}
+            </ul>
+        </>
     );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+    const firstCategory = 0;
+    const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',{
+        firstCategory
+    });
+    return {
+        props: {
+            menu,
+            firstCategory
+        }
+    };
+}
+
+interface HomeProps extends Record<string, unknown>{
+    menu: MenuItem[];
+    firstCategory: number;
+}
