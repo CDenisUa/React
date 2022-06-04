de996009780414bc772e9505c5ba7284a33b462f
CDenis <chepigadeveloper@gmail.com>
2022-06-04 23:14:45 +0200

    Starting to create an applicationэ

--- admin-dashboard/src/index.js
+++ admin-dashboard/src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+// Core
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// Components
+import App from './App';
+// Styles
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 
