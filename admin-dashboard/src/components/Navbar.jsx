d4914fe4ac118a18581960fe5d79dac4bf58738a
CDenis <chepigadeveloper@gmail.com>
2022-06-10 20:16:06 +0200

    Add user menu

--- admin-dashboard/src/components/Navbar.jsx
+++ admin-dashboard/src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 // Core
-import React, { useEffect } from 'react';
+import React from 'react';
+// Components
+import { Cart, Chat, Notification, UserProfile } from '.';
 // Context
 import { useStateContext } from '../contexts/ContextProvider';
 // Images
@@ -10,7 +12,6 @@ import { RiNotification3Line } from 'react-icons/ri';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import avatar from '../data/avatar.jpg';
-import { Cart, Chat, Notification, UserProfile } from '.';
 
 const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
   <TooltipComponent content={title} position='BottomCenter'>
@@ -30,8 +31,8 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 );
 
 const Navbar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
-
+  const { activeMenu, setActiveMenu, isClicked, setIsClicked,
+    handleClick } = useStateContext();
   return (
     <div className='flex justify-between p-2
     md:mx-6 relative'>
@@ -40,8 +41,62 @@ const Navbar = () => {
         color="blue"
         icon={<AiOutlineMenu />}
         customFunc={() => 
-          setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
-        />
+          setActiveMenu((prevActiveMenu) => !prevActiveMenu)
+        }
+      />
+        <div className='flex justify-between p-2 md:mx-6 relative'>
+          
+          <div className='flex'>
+            <NavButton 
+              title='Cart' 
+              color="blue"
+              icon={<FiShoppingCart />}
+              customFunc={() => handleClick('cart')}
+            />
+            <NavButton 
+              title='Chat'
+              dotColor="#03c9D7"
+              color="blue"
+              icon={<BsChatLeft />}
+              customFunc={() => 
+                setActiveMenu(handleClick('chat'))
+              }
+            />
+            <NavButton 
+              title='Notifications'
+              dotColor="#03c9D7"
+              color="blue"
+              icon={<RiNotification3Line />}
+              customFunc={() => setActiveMenu(handleClick('notification'))}
+            />
+            <TooltipComponent 
+              content="Profile"
+              position="BottomCenter"
+            >
+              <div 
+              className='flex items-center gap-2
+                cursor-pointer p-1 hover:bg-light-gray
+                rounded-lg'
+              onClick={() => handleClick('userProfile')}
+              >
+                <img 
+                  className="rounded-full w-8 h-8 object-cover"
+                  src={avatar}
+                />
+                <p>
+                  <span className='text-gray-400 text-14'>Hi,</span>
+                  {' '}
+                  <span className='text-gray-400 font-bold ml-1 text-14'>Denys</span>
+                </p>
+                <MdKeyboardArrowDown className='text-gray-400 text-14' />
+              </div>
+            </TooltipComponent>
+            { isClicked.cart && <Cart /> }
+            { isClicked.chat && <Chat /> }
+            { isClicked.notification && <Notification /> }
+            { isClicked.userProfile && <UserProfile /> }
+          </div>
+        </div>
     </div>
   )
 }
