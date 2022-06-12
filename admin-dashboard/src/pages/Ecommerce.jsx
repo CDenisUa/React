5281a24c83cde707262ce95185264b64ab33be4d
CDenis <chepigadeveloper@gmail.com>
2022-06-12 22:45:08 +0200

    Finishing up work on component SparkLine

--- admin-dashboard/src/pages/Ecommerce.jsx
+++ admin-dashboard/src/pages/Ecommerce.jsx
@@ -154,13 +154,22 @@ const Ecommerce = () => {
                     color='blue'
                   />
                 </div>
+                <div className='mt-10'>
+                  <Button
+                    color='white'
+                    bgColor='blue'
+                    text='Download Report'
+                    borderRadius='10px'
+                  />
+
+                </div>
               </div>
             </div>
         </div>
 
       </div>
       {/* Footer blocks end */}
-    </div>
+    </div> 
   )
 }
 
