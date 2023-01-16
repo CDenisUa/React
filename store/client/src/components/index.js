4e716ceb25eb281bb7f40003343c0da932a4e580
CDenis <chepigadeveloper@gmail.com>
2023-01-16 21:58:51 +0100

    Layout of Navbar

--- store/client/src/components/index.js
+++ store/client/src/components/index.js
@@ -1,4 +1,3 @@
 export { default as App } from './app/App';
 export { default as Navbar } from './navbar/Navbar';
 export { default as Footer } from './footer/Footer';
-export { default as Layout } from './layout/Layout';
--- store/client/src/components/navbar/Navbar.jsx
+++ store/client/src/components/navbar/Navbar.jsx
@@ -1,9 +1,44 @@
 // Core
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
+// Icons
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SearchIcon from '@mui/icons-material/Search';
+// import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+// import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+// import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className="left">
+        <div className="item">
+          <img src="/images/en.png" alt="lang" />
+          <KeyboardArrowDownIcon />
+        </div>
+        <div className="item">
+          <span>USD</span>
+          <KeyboardArrowDownIcon />
+        </div>
+        <div className="item">
+          <Link to='/products/1'> Women </Link>
+          <Link to='/products/2'> Men </Link>
+          <Link to='/products/3'> Children </Link>
+        </div>
+      </div>
+      <div className="center">
+        <Link to='/'>Super Store</Link>
+      </div>
+      <div className="right">
+        <Link to='/'> Home Page </Link>
+        <Link to='/'> About </Link>
+        <Link to='/'> Contact </Link>
+        <Link to='/'> Stores </Link>
+        <div className="icons">
+          <SearchIcon />
+        </div>
+      </div>
+    </div>
   )
 }
 
