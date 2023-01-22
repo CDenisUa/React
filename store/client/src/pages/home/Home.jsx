6f3581c533561fa17ce2e372ba07c6a48405fe78
CDenis <chepigadeveloper@gmail.com>
2023-01-22 18:40:56 +0100

    Add custom slider

--- store/client/src/pages/home/Home.jsx
+++ store/client/src/pages/home/Home.jsx
@@ -1,9 +1,15 @@
 // Core
 import React from 'react';
+// Styles
+import './Home.scss';
+// Components
+import { Slider } from 'components';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <Slider />
+    </div>
   )
 }
 
