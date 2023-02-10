a70b747a344bfd43f05fc930a371cef4b3b8a7f9
CDenis <chepigadeveloper@gmail.com>
2023-02-10 01:21:49 +0100

    Change name of style file

--- ui-kit/src/main.tsx
+++ ui-kit/src/main.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'
 // Components
 import App from './components/App/App'
+// Styles
+import '@/styles/main.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
