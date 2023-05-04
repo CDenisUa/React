ccbda5c9e0a824b40e3d4c4ad34a3b57d99048a3
CDenis <chepigadeveloper@gmail.com>
2023-05-04 23:25:58 +0200

    Add GET current user api

--- employees/controllers/users.js
+++ employees/controllers/users.js
@@ -2,6 +2,8 @@ const { prisma } = require('../prisma/prisma-client');
 const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken');
 
+const secret = process.env.JWT_SECRET;
+
 /**
  * @route POST /api/user/login
  * @desc User login
@@ -22,11 +24,12 @@ const login = async (req, res,) => {
 
     const isPasswordCorrect = user && (await bcrypt.compare(password, user.password));
 
-    if(user && isPasswordCorrect) {
+    if(user && isPasswordCorrect && secret) {
         res.status(200).json({
             id: user.id,
             email: user.email,
             name: user.name,
+            token:  jwt.sign({ id: user.id }, secret, { expiresIn: "30d" })
         });
     } else {
         return res.status(400).json('Wrong login or password!')
@@ -74,9 +77,7 @@ const register = async (req, res) => {
         password: hashedPassword,
       },
     });
-  
-    const secret = process.env.JWT_SECRET;
-  
+    
     if (user && secret) {
       res.status(201).json({
         id: user.id,
@@ -89,8 +90,14 @@ const register = async (req, res) => {
     }
 };
 
+/**
+ * @route GET /api/user/current
+ * @desc Current user
+ * @access Privet
+*/
+
 const current = async (req, res,) => {
-    res.send('current');
+    return res.status(200).json(req.user);
 }
 
 module.exports = {
