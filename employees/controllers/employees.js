1532676f8ef0ab67e288c06b2a7559b7071123ff
CDenis <chepigadeveloper@gmail.com>
2023-05-09 01:08:59 +0200

    Finish back end

--- employees/controllers/employees.js
+++ employees/controllers/employees.js
@@ -1,5 +1,5 @@
 // Core
-const { prisma } = require('../prisma/prisma-client');
+const { prisma } = require("../prisma/prisma-client");
 
 /**
     @route GET /api/employees
@@ -7,28 +7,38 @@ const { prisma } = require('../prisma/prisma-client');
     @access Privet
 */
 
-const allEmployees = async (req, res ) => {
-    try {
-        const employees = await prisma.employee.findMany();
-        res.status(200).json(employees);
-    } catch {
-        res.status(400).json({ message: 'Unable to get employees'})
-    } 
-}
+const allEmployees = async (req, res) => {
+  try {
+    const employees = await prisma.employee.findMany();
+    res.status(200).json(employees);
+  } catch {
+    res.status(400).json({ message: "Unable to get employees" });
+  }
+};
 
 /**
-    @route GET /api/employee
-    @desc  Get all employees
+    @route GET /api/employees/:id
+    @desc  Get one employee
     @access Privet
 */
 
 const getOneEmployee = async (req, res) => {
-    try{
-        console.log('Get one employee')
-    } catch {
-        res.status(500).json({ message: 'Something went wrong!'});
+  try {
+    const { id } = req.params;
+    const employee = await prisma.employee.findUnique({
+      where: {
+        id,
+      },
+    });
+    if(!employee) {
+        res.status(404).json({ message: 'User not found!'});
+    } else {
+        res.status(200).json(employee);
     }
-}
+  } catch {
+    res.status(500).json({ message: "Something went wrong!" });
+  }
+};
 
 /**
     @route POST /api/employees/add
@@ -37,60 +47,79 @@ const getOneEmployee = async (req, res) => {
 */
 
 const addEmployee = async (req, res) => {
-    try {
-        const data = req.body;
+  try {
+    const data = req.body;
 
-        if(!data.firstName || !data.lastName || !data.address || !data.age) {
-            return res.status(400).json({ message: 'All fields must be filled out!'})
-        }
-
-        const employee = await prisma.employee.create({
-            data : {
-                ...data,
-                userId: req.user.id
-            }
-        });
-        
-        
-
-        return res.status(201).json(employee);
-    } catch {
-        res.status(500).json({ message: 'Something went wrong!'});
+    if (!data.firstName || !data.lastName || !data.address || !data.age) {
+      return res
+        .status(400)
+        .json({ message: "All fields must be filled out!" });
     }
-}
+
+    const employee = await prisma.employee.create({
+      data: {
+        ...data,
+        userId: req.user.id,
+      },
+    });
+
+    return res.status(201).json(employee);
+  } catch {
+    res.status(500).json({ message: "Something went wrong!" });
+  }
+};
 
 /**
-    @route GET /api/employee
-    @desc  Get all employees
+    @route Delete /api/employees/remove/:id
+    @desc  Remove employee
     @access Privet
 */
 
 const removeEmployee = async (req, res) => {
-    try{
-        console.log('Get one employee')
-    } catch {
-        res.status(500).json({ message: 'Something went wrong!'});
-    }
-}
+  try {
+    const { id } = req.body;
+    await prisma.employee.delete({
+      where: {
+        id,
+      },
+    });
+
+    res.status(204).json({ message: "User was successfully deleted!" });
+  } catch {
+    res.status(500).json({ message: "Something went wrong!" });
+  }
+};
 
 /**
-    @route GET /api/employee
-    @desc  Get all employees
+    @route PUT /api/employees/edit/:id
+    @desc  Update employee
     @access Privet
 */
 
 const updateEmployee = async (req, res) => {
-    try{
-        console.log('Get one employee')
-    } catch {
-        res.status(500).json({ message: 'Something went wrong!'});
-    }
-}
+  try {
+    const data = req.body;
+    const id = data.id;
+
+    await prisma.employee.update({
+      where: {
+        id,
+      },
+      data: data,
+    });
+
+    res
+      .status(20)
+      .json({ message: 'Employee updated successfully' });
+  } catch {
+    res.status(500).json({ message: "Something went wrong!" });
+  }
+};
 
 module.exports = {
-    allEmployees,
-    getOneEmployee,
-    addEmployee,
-    removeEmployee,
-    updateEmployee
-}
+  allEmployees,
+  getOneEmployee,
+  addEmployee,
+  removeEmployee,
+  updateEmployee,
+};
